28029b4e27f28b247a499e96d3a6f3dcceeadf69
jbwwboom <justinvdboomen@gmail.com>
2018-10-06 03:15:05 +0200

    Made socketIO client in iOS.

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WebsocketAppiOS' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WebsocketAppiOS
+	pod 'Socket.IO-Client-Swift', '~> 13.3.0'
+
+  target 'WebsocketAppiOSTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WebsocketAppiOSUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- WebsocketAppiOS/ViewController.swift
+++ WebsocketAppiOS/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import SocketIO
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var orientationLabel: UILabel!
+    let manager = SocketManager(socketURL: URL(string: "http://192.168.178.24:3000")!, config: [.log(true), .compress])
+    var socket : SocketIOClient!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.socket = manager.defaultSocket
+        
+        self.socket.on("orientation"){data, ack in
+            if let bool = data[0] as? String {
+                self.changeOrientation(value: bool.toBool()!)
+            }
+        }
+        
+        self.socket.connect()
+        
+        getOrientation()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //To be sure it gets send on startup.
+        getOrientation()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        getOrientation()
+    }
+    
+    func getOrientation(){
+        var orientation = ""
+        if(UIDevice.current.orientation.isLandscape){
+            orientation = "Landscape"
+            sendOrientation(orientation: orientation)
+        }else if(UIDevice.current.orientation.isPortrait){
+            orientation = "Portrait"
+            sendOrientation(orientation: orientation)
+        }
+    }
+    
+    func changeOrientation(value : Bool){
+        if(value){
+            self.view.backgroundColor = UIColor.green
+            orientationLabel.text = "CORRECT"
+        }else{
+            self.view.backgroundColor = UIColor.red
+            orientationLabel.text = "FALSE"
+        }
+    }
+    
+    func sendOrientation(orientation: String){
+        self.socket.emit("change orientation", orientation)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.socket.disconnect()
+    }
+}
 
-
+extension String {
+    func toBool() -> Bool? {
+        switch self {
+        case "True", "true", "yes", "1":
+            return true
+        case "False", "false", "no", "0":
+            return false
+        default:
+            return nil
+        }
+    }
 }
 
